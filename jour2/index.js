8c82738c1f0a297a7f400b2d2270f1c73c89c595
Aurélie VIOLETTE <aurelie.violette@purple-fox.fr>
2021-12-02 13:25:08 +0100

    Jour 2

---
+++ jour2/index.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const path = require('path')
+const ora = require('ora')
+const chalk = require('chalk')
+
+function updatePosition(position, { command, input }) {
+  if (command === 'forward') {
+    position.x = position.x + input
+  } else if (command === 'down') {
+    position.y = position.y + input
+  } else if (command === 'up') {
+    position.y = Math.max(0, position.y - input)
+  }
+}
+
+function executeInstructions(instructions) {
+  const position = { x: 0, y: 0 }
+  for (let instruction of instructions) {
+    updatePosition(position, instruction)
+  }
+
+  return position
+}
+
+function main() {
+  console.time('exec')
+  const resolving = ora('Reading file').start()
+  //   const rawInput = fs.readFileSync(path.resolve(__dirname, 'test.txt'), 'utf-8')
+  const rawInput = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf-8')
+
+  const instructions = rawInput.split('\n').map((line) => {
+    const [, command, input] = line.match(/^(\D+) (\d+)$/)
+    return { command, input: parseInt(input) }
+  })
+
+  const finalPosition = executeInstructions(instructions)
+
+  resolving.succeed(`Jour ${chalk.red(2)} - the answer is ${chalk.bold.magenta(finalPosition.x * finalPosition.y)}`)
+  console.timeEnd('exec')
+}
+
+main()
